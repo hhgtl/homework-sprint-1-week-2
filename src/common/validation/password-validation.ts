402cc54397c68c377853bb4841f61bf7857cd5f0
petro <petrosahal66@gmail.com>
2026-02-09 21:21:47 +0200

    fix

--- src/common/validation/password-validation.ts
+++ src/common/validation/password-validation.ts
@@ -3,5 +3,5 @@ import {body} from "express-validator";
 export const passwordValidation = body('password')
     .isString()
     .trim()
-    .isLength({ min: 6, max: 10 })
+    .isLength({ min: 6, max: 20 })
     .withMessage('Password must be at least 3 characters and not long 10 characters')
