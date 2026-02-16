a0f753ab7d5b8f7ec3cecb684ca0e2ec03af8481
petro <petrosahal66@gmail.com>
2026-02-16 14:37:49 +0200

    feat

--- src/modules/comments/validation/comments-content-validation.ts
+++ src/modules/comments/validation/comments-content-validation.ts
@@ -3,5 +3,5 @@ import {body} from "express-validator";
 export const commentsContentValidation = body('content')
     .isString()
     .trim()
-    .isLength({ min: 20, max: 500 })
-    .withMessage('Content should be a string with max length 500')
+    .isLength({ min: 20, max: 300 })
+    .withMessage('Content should be a string with max length 300')
