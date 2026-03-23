33ef5b9e7e1ba7205f48cb1156f8725fbc7ffe6e
petro <petrosahal66@gmail.com>
2026-03-23 14:42:12 +0200

    check

--- src/modules/rate-limit/domain/rate-limit-service.ts
+++ src/modules/rate-limit/domain/rate-limit-service.ts
@@ -4,12 +4,13 @@ import {rateLimitRepositories} from "../infrastructure/rate-limit-repositories";
 export const rateLimitService = {
     async checkRateLimit({ip, url}: {url: string, ip: string}) {
         const payload = await rateLimitRepositories.findByIpAndUrl({ip, url});
+        const timeLimit = Date.now() - 10000;
 
-        const checkDate = payload.every(({date}) => {
-            return new Date(date).getTime() <= (Date.now() - 10000)
+        const recentRequests = payload.filter(({date}) => {
+            return new Date(date).getTime() > timeLimit;
         });
 
-        return payload.length < 5 && checkDate;
+        return recentRequests.length < 5;
     },
     async addToRateLimit({ip, url}: {url: string, ip: string}) {
         const data: RateLimitDbType = {
