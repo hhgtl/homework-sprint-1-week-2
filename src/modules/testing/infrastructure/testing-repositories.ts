26b3822ffa156368c35f5d71bb56b92ac3b1e1e3
petro <petrosahal66@gmail.com>
2026-03-23 14:25:43 +0200

    check

--- src/modules/testing/infrastructure/testing-repositories.ts
+++ src/modules/testing/infrastructure/testing-repositories.ts
@@ -1,4 +1,11 @@
-import {blogsCollection, commentsCollection, postsCollection, usersCollection} from "../../../db/db";
+import {
+    blogsCollection,
+    commentsCollection,
+    postsCollection,
+    rateLimitCollection,
+    usersCollection
+} from "../../../db/db";
+
 
 export const testingRepositories = {
     removeAllData: async () => {
@@ -6,5 +13,6 @@ export const testingRepositories = {
         await blogsCollection.deleteMany()
         await usersCollection.deleteMany()
         await commentsCollection.deleteMany()
+        await rateLimitCollection.deleteMany()
     }
 }
