54eb2847933028e3ef856118f26e4a949c685afd
petro <petrosahal66@gmail.com>
2026-04-08 18:17:20 +0300

    fix

--- src/modules/auth/constants/token-constants.ts
+++ src/modules/auth/constants/token-constants.ts
@@ -2,5 +2,6 @@ export const REFRESH_TOKEN = "refreshToken"
 
 export const ACCESS_TOKEN_EXPIRED = "10s"
 export const REFRESH_TOKEN_EXPIRED = "20s"
-export const COOKIE_MAX_AGE_20_SECONDS = 200 * 1000
+// export const COOKIE_MAX_AGE_20_SECONDS = 200 * 1000
+export const COOKIE_MAX_AGE_20_SECONDS = 24 * 60 * 60 * 1000
 // export const COOKIE_MAX_AGE_20_SECONDS = 20 * 1000
