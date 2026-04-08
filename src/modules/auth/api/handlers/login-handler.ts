fe35d3c241e1cb93ac294a1ce01820a80e8492ec
petro <petrosahal66@gmail.com>
2026-04-08 19:07:16 +0300

    test

--- src/modules/auth/api/handlers/login-handler.ts
+++ src/modules/auth/api/handlers/login-handler.ts
@@ -20,7 +20,7 @@ export const loginHandler = async (req: Request, res: Response) => {
     }
 
     console.log("REFRESH TOKEN VALUE:", user.data?.refreshToken);
+    console.log("ACCESS TOKEN VALUE:", user.data?.accessToken);
     res.cookie(REFRESH_TOKEN, user.data?.refreshToken, {httpOnly: true, secure: true, maxAge: COOKIE_MAX_AGE_20_SECONDS, sameSite: 'none', path: '/'})
-    res.cookie("accessToken", user.data?.accessToken, {httpOnly: true, secure: true, maxAge: COOKIE_MAX_AGE_20_SECONDS, sameSite: 'none', path: '/'})
     res.status(HttpStatuses.Success).send({accessToken: user.data?.accessToken})
 }
